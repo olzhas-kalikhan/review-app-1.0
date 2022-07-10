9464e5ce5a08b2b1328ea2442678e7c5f71cb397
Olzhas Kalikhan <olzhas828@gmail.com>
2022-07-09 23:53:16 -0400

    removed auth utils

--- src/server/createContext.ts
+++ src/server/createContext.ts
@@ -1,6 +1,5 @@
 import { prisma } from "@/utils/prisma";
 import * as trpc from "@trpc/server";
-import { getUserFromRequest } from "@/utils/auth";
 import { NextApiRequest, NextApiResponse } from "next";
 
 export async function createContext({
@@ -10,8 +9,7 @@ export async function createContext({
   req: NextApiRequest;
   res: NextApiResponse;
 }) {
-  const user = await getUserFromRequest(req);
-  return { req, res, prisma, user };
+  return { req, res, prisma };
 }
 
 export type Context = trpc.inferAsyncReturnType<typeof createContext>;
