94a87e6c963aeea05bf761744b1aca9a0f231806
Olzhas Kalikhan <olzhas828@gmail.com>
2022-07-10 00:10:53 -0400

    remove auth router

--- src/server/router/app.router.ts
+++ src/server/router/app.router.ts
@@ -1,10 +1,7 @@
 import { createRouter } from "../createRouter";
-import { authRouter } from "./auth.router";
 import { userRouter } from "./user.router";
 
-export const appRouter = createRouter()
-  .merge("users.", userRouter)
-  .merge("auth.", authRouter);
+export const appRouter = createRouter().merge("users.", userRouter);
 // export type definition of API
 export type AppRouter = typeof appRouter;
 
