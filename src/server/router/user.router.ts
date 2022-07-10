708e0c61c74b0599274007c7f4663d18800c4cc9
Olzhas Kalikhan <olzhas828@gmail.com>
2022-07-10 00:21:10 -0400

    removed user from ctx

--- src/server/router/user.router.ts
+++ src/server/router/user.router.ts
@@ -4,43 +4,37 @@ import { createUserSchema } from "../../schema/user.schema";
 import { createRouter } from "../createRouter";
 import bcrypt from "bcrypt";
 
-export const userRouter = createRouter()
-  .query("me", {
-    resolve({ ctx }) {
-      return ctx.user;
-    },
-  })
-  .mutation("register-user", {
-    input: createUserSchema,
-    async resolve({ ctx, input }) {
-      const { email, name, password } = input;
+export const userRouter = createRouter().mutation("register-user", {
+  input: createUserSchema,
+  async resolve({ ctx, input }) {
+    const { email, name, password } = input;
 
-      try {
-        const hashedPassword = await bcrypt.hash(password, 10);
-        const user = await ctx.prisma.user.create({
-          data: {
-            email,
-            name,
-            password: hashedPassword,
-          },
-        });
+    try {
+      const hashedPassword = await bcrypt.hash(password, 10);
+      const user = await ctx.prisma.user.create({
+        data: {
+          email,
+          name,
+          password: hashedPassword,
+        },
+      });
 
-        return user;
-      } catch (e) {
-        if (e instanceof PrismaClientKnownRequestError) {
-          if (e.code === "P2002") {
-            throw new trpc.TRPCError({
-              code: "CONFLICT",
-              message: "User already exists",
-            });
-          }
+      return user;
+    } catch (e) {
+      if (e instanceof PrismaClientKnownRequestError) {
+        if (e.code === "P2002") {
+          throw new trpc.TRPCError({
+            code: "CONFLICT",
+            message: "User already exists",
+          });
         }
-
-        throw new trpc.TRPCError({
-          code: "INTERNAL_SERVER_ERROR",
-          message: "Something went wrong",
-          cause: e,
-        });
       }
-    },
-  });
+
+      throw new trpc.TRPCError({
+        code: "INTERNAL_SERVER_ERROR",
+        message: "Something went wrong",
+        cause: e,
+      });
+    }
+  },
+});
